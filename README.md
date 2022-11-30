7c8f8e58560da8576d2872bfea190faa5e698ded
Jonathan <IndiaJonathan@gmail.com>
2022-11-30 11:30:37 -0600

    Increasing odds and adding snapshot 2

--- README.md
+++ README.md
@@ -2,6 +2,25 @@
 
 This script is used to select winning wallets out of a csv file, and add them to an output csv file.
 
+Rates
+Common - 51%
+Uncommon - 22.5% -> 26.5%
+Rare - 14%
+Epic - 8%
+Legendary - 4%
+Ancient - 0.5%
+
+20 % chance for every option
+
+So Common Chance is .51 * .20 = 0.102
+
+
+Wallets Removed From Airdrop List (Gala wallets):
+- 0xaa94d963bd7b6c8ddde07a4b4cf13e26c2650899
+- 0xaa1d6169247dc28ac8f09b8c692b865aa423b06a
+- 0x5171dcfd09ac42f591f9bb90bc9a330cd0cc0d94
+- 0xaa94d963bd7b6c8ddde07a4b4cf13e26c2650899
+
 Transactions will be obtained from chainlink contract: https://etherscan.io/address/0xf908b478019201db8f3b7e1379ef14d4f64d0fcf
 
 Snapshot 1 Seed
@@ -9,10 +28,26 @@ Snapshot 1 Seed
 52694531950886193989452151201864687472399892647089387333720639450868763087547
 https://etherscan.io/tx/0x1aa7938009c058b3d76efd30d4db9ddc75239fcad0096e42c4fdf8c16e525e05
 
+Snapshot 2 Seed
+---------------------------------------------------------------------------------
+108117904175788122750336416322539293449614988016422674797095535283761033173437
+https://etherscan.io/tx/0xc4faaac4559a91898a28c8b0dca8670c6c2b786da3e482dc38424cdba042165a
 
 
 
+To run Snapshot_1 (Commons) Taken 11/21/2022:
 ```bash
-To run Snapshot_1 (Commons):
     npm start -- --folder ./Snapshot_1 --weight 0.102 --random 52694531950886193989452151201864687472399892647089387333720639450868763087547
+```
+
+
+To run Snapshot_2 (Uncommons) Taken 11/30/2022:
+Old
+```bash
+    npm start -- --folder ./Snapshot_2 --weight 0.045 --random 108117904175788122750336416322539293449614988016422674797095535283761033173437
+```
+
+To run Snapshot_2 Increased Odds (Uncommons) Taken 11/30/2022:
+```bash
+    npm start -- --folder ./Snapshot_2 --weight 0.053 --random 108117904175788122750336416322539293449614988016422674797095535283761033173437
 ```
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ import { createReadStream, createWriteStream } from "fs";
 import seedrandom from "seedrandom";
 
 let totalBalance = 0;
+let totalBalanceMinusGala = 0;
+
+const galaWallets = [
+  "0x5c5f2d6b503ff195cb989cbcfe73d92858c491a9",
+  "0xaa1d6169247dc28ac8f09b8c692b865aa423b06a",
+  "0x5171dcfd09ac42f591f9bb90bc9a330cd0cc0d94",
+  "0xaa94d963bd7b6c8ddde07a4b4cf13e26c2650899",
+];
 
 program.option("-r, --random <number>", "random number from chainlink");
 program.option("-f, --folder <type>", "path to snapshots");
@@ -22,11 +30,15 @@ const snapshotTrolls: { wallet: string; balance: number }[] = [];
 const snapshotBoxes: { wallet: string; balance: number }[] = [];
 const winners: { [wallet: string]: number } = {};
 let winnerCount = 0;
+let winnerCountMinusGala = 0;
 
 createReadStream(`${opts.folder}/trolls_vox_balance.csv`)
   .pipe(csvParser())
   .on("data", (data) => {
     totalBalance += Number(data.balance);
+    if (!isGalaWallet(data.wallet)) {
+      totalBalanceMinusGala += Number(data.balance);
+    }
     snapshotTrolls.push({ wallet: data.wallet, balance: data.balance });
   })
   .on("end", () => {
@@ -38,6 +50,9 @@ createReadStream(`${opts.folder}/trolls_vox_balance.csv`)
       .pipe(csvParser())
       .on("data", (data) => {
         totalBalance += Number(data.balance);
+        if (!isGalaWallet(data.wallet)) {
+          totalBalanceMinusGala += Number(data.balance);
+        }
         snapshotBoxes.push({ wallet: data.wallet, balance: data.balance });
       })
       .on("end", () => {
@@ -56,16 +71,51 @@ createReadStream(`${opts.folder}/trolls_vox_balance.csv`)
         console.log("Winners written to file: ", winnerFile);
         console.log("Total Entries: ", totalBalance);
         console.log("Total Winners: ", winnerCount);
+        console.log("Average Expected: ", totalBalance * opts.weight);
+
+        createReadStream(`${opts.folder}/winners.csv`)
+        .pipe(csvParser())
+        .on("data", (data) => {
+          totalBalance += Number(data.balance);
+          if (isGalaWallet(data.wallet)) {
+            totalBalanceMinusGala += Number(data.balance);
+          }
+          snapshotBoxes.push({ wallet: data.wallet, balance: data.balance });
+        })
+        .on("end", () => {
+          const output = Object.entries(winners).filter(winner => !isGalaWallet(winner[0])).map(
+            (winner) => `${winner[0]},${winner[1]}`
+          );
+          //Add headers
+          output.unshift("address,quantity");
+          const airDropFile = `${opts.folder}/airdrop-list.csv`;
+          createWriteStream(airDropFile).write(output.join("\n"));
+          console.log("Airdrop written to file: ", winnerFile);
+          console.log("Net Entries: ", totalBalanceMinusGala);
+          console.log("Net Winners: ", winnerCountMinusGala);
+          console.log("Net Expected: ", totalBalanceMinusGala * opts.weight);
+        })
+
       });
   });
 
 const updateWinner = (address: string, balance: number) => {
+  const isGala = isGalaWallet(address);
   let wins = winners[address] || 0;
   for (let index = 0; index < balance; index++) {
     if (RNG() <= opts.weight) {
       wins++;
       winnerCount++;
+      if (!isGala){
+        winnerCountMinusGala++;
+      }
     }
     if (wins) winners[address] = wins;
   }
+};
+
+const isGalaWallet = (wallet: string) => {
+  return !!galaWallets.find(
+    (gwallet) => gwallet.toLowerCase() == wallet.toLowerCase()
+  );
 };
